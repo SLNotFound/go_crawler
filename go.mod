c7ff6636b95b254fbaf8635bffb78edd7d1f1789
SunLightLei <1255193087@qq.com>
2022-07-31 20:41:24 +0800

    获取页面

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
-module "go_crawler"
+module go_crawler
+
+go 1.17
+
+require (
+	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/transform"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	e := determineEncoding(resp.Body)
+	//utf8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+	all, err := ioutil.ReadAll(utf8Reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", all)
+}
+
+// 根据页面的charset 自动转换中文
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
