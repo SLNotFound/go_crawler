15b89c9e8e1182b2bc4fe789a7212a0ff6ab2cfd
SunLightLei <1255193087@qq.com>
2022-08-06 17:11:56 +0800

    优化整体结构

---
+++ zhenai/parser/cityList.go
@@ -0,0 +1,23 @@
+package parser
+
+import (
+	"go_crawler/engine"
+	"regexp"
+)
+
+const cityListReg = `<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"[^>]*>([^<]+)</a>`
+
+func ParseCityList(contents []byte) engine.ParseResult {
+	re := regexp.MustCompile(cityListReg)
+	matches := re.FindAllSubmatch(contents, -1)
+
+	result := engine.ParseResult{}
+	for _, m := range matches {
+		result.Items = append(result.Items, string(m[2]))
+		result.Requests = append(result.Requests, engine.Request{
+			Url:        string(m[1]),
+			ParserFunc: engine.NilParser,
+		})
+	}
+	return result
+}
