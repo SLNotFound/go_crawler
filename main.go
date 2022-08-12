422a4707600569e24e2c362350d8c3aa483e3a5f
sunl <1255193087@qq.com>
2022-08-12 17:38:46 +0800

    。。。

--- main.go
+++ main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"go_crawler/fetcher"
+	"go_crawler/engine"
+	"go_crawler/zhenai/parser"
 )
 
-//func main() {
-//	url := "http://www.zhenai.com/zhenghun"
-//	engine.Run(engine.Request{
-//		Url:        url,
-//		ParserFunc: parser.ParseCityList,
-//	})
-//}
-
 func main() {
-	//url := "https://album.zhenai.com/u/1213676771"
-	url := "https://album.zhenai.com/u/1213676771"
-	contents, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Printf("fetch url err: %v\n", err)
-	}
-
-	fmt.Println(contents)
+	url := "http://www.zhenai.com/zhenghun"
+	engine.Run(engine.Request{
+		Url:        url,
+		ParserFunc: parser.ParseCityList,
+	})
 }
